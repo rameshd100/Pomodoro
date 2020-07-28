f46d177946eb16e36a7c228e15973f89e2643dc1
rameshd100 <rameshdahal2013@gmail.com>
2020-07-28 19:30:31 -0400

    start, stop, and reset functionality added

---
+++ src/components/Pomodoro.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from "react";
+import "./Pomodoro.css";
+
+const Pomodoro = () => {
+  const [title, setTitle] = useState("Let the countdown begain".toUpperCase());
+  const [timeLeft, setTimeLeft] = useState(30 * 60);
+  const intervelRef = useRef(null);
+  // Functions
+
+  // Start timer Functionn
+  const startTimer = () => {
+    intervelRef.current = setInterval(() => {
+      return setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+
+        // RESET THE TIMER
+
+        return 0;
+      });
+    }, 1000);
+  };
+  //Restart Timer Function
+  const resetTimer = () => {
+    return setTimeLeft(30 * 60);
+  };
+
+  // Stop Timer
+  const stopTimer = () => {
+    clearInterval(intervelRef.current);
+  };
+
+  const minutes = Math.floor(timeLeft / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (timeLeft - minutes * 60).toString().padStart(2, "0");
+
+  // Padstart is a JS function to add number in the beganning to another number
+
+  return (
+    <div className="pomodoro">
+      <h2>{title}</h2>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="buttons">
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Restart</button>
+      </div>
+    </div>
+  );
+};
+
+export default Pomodoro;
